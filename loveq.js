0454de545ea88171596858595b3725c72dd30b31
gitHub709394 <31923187+gitHub709394@users.noreply.github.com>
2021-01-04 18:04:28 +0800

    Update loveq.js
    update author

--- loveq.js
+++ loveq.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Xiang
 // @match        https://www.loveq.cn/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_log
